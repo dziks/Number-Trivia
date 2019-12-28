c438f26a66c61a5a9c6c05d22a053df466feeae5
Francis Dzikunu <francisdziks@gmail.com>
2019-12-28 13:53:32 +0000

    fixed properties

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable{
-  Failure([List properties = const<dynamic>[]]): super();
+  Failure([List properties = const<dynamic>[]]): super(properties);
 }
 
 //General Failures
